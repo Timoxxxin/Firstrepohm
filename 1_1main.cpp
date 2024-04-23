b0059ee7f472b68e88bc9ad359d39efbc3ab01b2
Тимофей Гладких <gladkihtimofey14@gmail.com>
2024-04-23 12:46:26 +0300

    Теперь ф-ия fib(n) вычисляет значения с 0

--- 1_1main.cpp
+++ 1_1main.cpp
@@ -5,11 +5,22 @@ int fib(int n)
 {
 	if (n<=1)
 		return n;
-	else
-		return fib(n - 1) + fib(n - 2);
+	else {
+	int first = 0;
+	int second = 1;
+	int next = 1;
+	for int (i = 2; i < n; i++){
+		next = first + second;
+		first = second;
+		second = next;
+	}
+	return next;
+  }
 }
 	int main(){
 	cout << "Hello world"; << endl;
-	cour << fib(10) << endl;
+	cout << "N = ";
+	cin >> n;
+	cout << "fib(n) = " << fib(n) << endl;
 	return 0;
 	}
