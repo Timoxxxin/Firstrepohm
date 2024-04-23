514a91cf667e1e9d236365a92238d0f7280eaba9
Тимофей Гладких <gladkihtimofey14@gmail.com>
2024-04-23 12:18:15 +0300

    Добавили ф-ию фиб

--- 1_1main.cpp
+++ 1_1main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int fib(int n)
 {
-	cout << "Hello world"; << endl;
-	return 0;
+	if (n<=1)
+		return n;
+	else
+		return fib(n - 1) + fib(n - 2);
 }
-
+	int main(){
+	cout << "Hello world"; << endl;
+	cour << fib(10) << endl;
+	return 0;
+	}
