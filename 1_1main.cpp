7ccb799750f40879773ad83fe1fda768e577cb9a
Тимофей Гладких <gladkihtimofey14@gmail.com>
2024-04-23 13:00:18 +0300

    Теперь ф-ия fib(n) раюотает итеративно

--- 1_1main.cpp
+++ 1_1main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int fib(int n)
+void fib(int n)
 {
-	if (n<=1)
-		return n;
-	else {
 	int first = 0;
 	int second = 1;
-	int next = 1;
-	for int (i = 2; i < n; i++){
-		next = first + second;
-		first = second;
-		second = next;
+	cout << "First" << n << ("Fib n") << endl;
+	for (int i = 0; i < n; i++){
+	 if (i <= 1)
+		 cout << i << endl;
+	else {
+	int  next = first + second;
+	cout << next << endl;
+	 first = second;
+	 second = next;
 	}
-	return next;
   }
 }
 	int main(){
+	int n;
 	cout << "Hello world"; << endl;
 	cout << "N = ";
 	cin >> n;
-	cout << "fib(n) = " << fib(n) << endl;
+	fib(n);
 	return 0;
 	}
